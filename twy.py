c2e35513471b73ac68104c3197713dc20be020f9
Mohd Bilal Hasan <bilalhasan403@gmail.com>
2025-02-24 01:20:30 +0530

    Delete twy.py

--- twy.py
+++
@@ -1,6 +0,0 @@
-db_info = {dbname: st.session_state["db_name"], 
-        user=st.session_state["db_user"],
-        password=st.session_state["db_password"],
-        host=st.session_state["db_host"]}
-
-print(**db_info)
